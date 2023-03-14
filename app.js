90d29b4b7eedcb3188c08f7c632024a5c70ad51f
Spencer Hansen <spencerhansen@gmail.com>
2023-03-14 14:00:20 -0600

    finished

--- app.js
+++ app.js
@@ -4,7 +4,32 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Spencer";
+let lastName;
+var age;
 
 // Exercise 2
+lastName = "Hansen";
+age = 26;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize ="m";  
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss"; 
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${title} the Author is ${author}`);
